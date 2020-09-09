ba78fd4596dea158cdd47ae9e29fdc395546b4f6
Matt Ellis <mellis@tidelift.com>
2020-09-08 23:38:44 -0400

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,5 @@
-(require 'package) ;; You might already have this line
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize) ;; You might already have this line
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -14,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-mode column-marker json-mode js2-mode web-mode flycheck yaml-mode dumb-jump rvm robe))))
+    (rspec-mode ag company go-mode column-marker json-mode js2-mode web-mode flycheck yaml-mode smart-jump rvm robe))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,14 +16,19 @@
  ;; If there is more than one, they won't work right.
  )
 (require 'flymake-ruby)
+(require 'rspec-mode)
+(global-set-key (kbd "C-M-g") 'dumb-jump-go)
+(setq rspec-use-rvm t)
+(setq ruby-version-manager 'rvm)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
-(dumb-jump-mode)
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
 (setq-default indent-tabs-mode nil)
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
 (setq js2-strict-missing-semi-warning nil)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jb\\'" . ruby-mode))
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
@@ -44,3 +43,7 @@
 
 (add-hook 'ruby-mode-hook 'whitespace-mode)
 (global-auto-revert-mode t)
+(set-face-attribute 'default nil :height 220)
+(load-theme 'nimbus t)
+(global-set-key [?\C-x ?\C-f] 'ag-project-dired)
+(put 'upcase-region 'disabled t)
